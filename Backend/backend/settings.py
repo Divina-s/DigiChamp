57b2881787c8589e3866e45337296bb835894a86
Divina Mbel <125366367+Divina-s@users.noreply.github.com>
2025-08-25 23:15:41 +0100

    Update settings.py 2

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # -----------------------------
 SECRET_KEY = config("SECRET_KEY", default="fallback-secret-key")
 DEBUG = config("DEBUG", default=False, cast=bool)
-ALLOWED_HOSTS = ["digichamp-backend.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["digichamp-backend.onrender.com"]
 CSRF_TRUSTED_ORIGINS = ["https://digichamp-backend.onrender.com"]
 
 # -----------------------------
