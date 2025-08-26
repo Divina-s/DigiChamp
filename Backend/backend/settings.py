4e3cdd98d2f313ec7988c9926e0877849136537a
Divina Mbel <125366367+Divina-s@users.noreply.github.com>
2025-08-26 07:05:39 +0100

    Update settings.py ddedd

--- Backend/backend/settings.py
+++ Backend/backend/settings.py
@@ -5,6 +5,9 @@ import os
 import dj_database_url
 from decouple import config
 
+print("DATABASE_URL:", config("DATABASE_URL", default="Not Found"))
+
+
 # -----------------------------
 # BASE
 # -----------------------------
